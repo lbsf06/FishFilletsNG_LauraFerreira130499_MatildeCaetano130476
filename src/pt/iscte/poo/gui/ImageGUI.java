b22c6ed1a8e32bbd896ead0c5f08c86fb2894585
Matilde Caetano <mmcoo4@iscte-iul.pt>
2025-11-19 19:01:20 +0000

    aaa
    aaaa

--- src/pt/iscte/poo/gui/ImageGUI.java
+++ src/pt/iscte/poo/gui/ImageGUI.java
@@ -11,8 +11,6 @@ import java.awt.event.KeyListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.awt.image.BufferedImage;
-// import java.awt.event.MouseEvent;
-// import java.awt.event.MouseListener;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
