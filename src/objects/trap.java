8a0b4e4c76c1010ba7180ae926ef175610f990cd
Matilde Caetano <mmcoo4@iscte-iul.pt>
2025-11-25 14:46:22 +0000

    a

---
+++ src/objects/trap.java
@@ -0,0 +1,16 @@
+package objects;
+
+public class trap extends StaticObject {
+
+    public trap(pt.iscte.poo.utils.Point2D p) {
+        super(p, "trap");
+    }
+
+    public boolean leve() {
+        return false;
+    }
+
+    public int getLayer() {
+        return 2;
+    }
+}
--- src/pt/iscte/poo/game/Main.java
+++ src/pt/iscte/poo/game/Main.java
@@ -40,3 +40,20 @@ public class Main {
         }
     }
 }
+
+/* package pt.iscte.poo.game;
+
+import pt.iscte.poo.gui.ImageGUI;
+
+public class Main {
+
+	public static void main(String[] args) {
+		ImageGUI gui = ImageGUI.getInstance();
+		GameEngine engine = new GameEngine();
+		gui.setStatusMessage("Good luck!");
+		gui.registerObserver(engine);
+		gui.go();
+	}
+	
+}
+ */
