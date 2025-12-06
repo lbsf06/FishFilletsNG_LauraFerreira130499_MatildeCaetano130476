9e41ae32b5629963d8dbb30fdf44db0768e7ebdc
lbsf06 <laurabsferreira06@gmail.com>
2025-12-06 21:25:24 +0000

    .

--- src/pt/iscte/poo/objects/GameObject.java
+++ src/pt/iscte/poo/objects/GameObject.java
@@ -3,34 +3,39 @@ package pt.iscte.poo.objects;
 import pt.iscte.poo.gui.ImageTile;
 import pt.iscte.poo.utils.Point2D;
 
-public abstract class GameObject implements ImageTile {
+public abstract class GameObject implements ImageTile { // O imageTile faz com que para obtermos a imagem tenhamos de
+                                                        // declarar a posição do objeto e o nome da imagem
 
-    protected Point2D position;
+    protected Point2D position; // As subclasses podem aceder a estes atributos
     protected String imageName;
 
-    public GameObject(Point2D p) {
+    public GameObject(Point2D p) { // O point2D p é a posição do objeto inicial
         this.position = p;
 
     }
 
-    public void setPosition(Point2D p) {
+    public void setPosition(Point2D p) { // Método para alterar a posição do objeto
         this.position = p;
     }
 
-    @Override
-    public String getName() {
+    public abstract TipoObjeto getTipo();// Método abstrato que obriga as subclasses a implementar o método getTipo, que
+                                         // retorna o tipo do objeto
+
+    @Override // sobrescreve o método getName da interface ImageTile
+    public String getName() { // Método getter da classe abstrata ImageTile que serve para obter o nome da
+                              // imagem do objeto
+        // A GUI vai chamar isto para saber que imagem desenhar.
         return imageName;
     }
 
-    @Override
-    public Point2D getPosition() {
-        return position;
+    @Override // sobrescreve o método getPosition da interface ImageTile
+    public Point2D getPosition() { // Método getter da classe abstrata ImageTile que serve para obter a posição do
+                                   // objeto
+        return position; // A GUI desenha a imagem aqui.
     }
 
-    public abstract TipoObjeto getTipo();
-
-    @Override
-    public int getLayer() {
+    @Override // sobrescreve o método getLayer da interface ImageTile
+    public int getLayer() { // Método da classe abstrata ImageTile
         return 1;
     }
 }
--- src/pt/iscte/poo/objects/StaticObject.java
+++ src/pt/iscte/poo/objects/StaticObject.java
@@ -3,34 +3,35 @@ package pt.iscte.poo.objects;
 import pt.iscte.poo.utils.Direction;
 import pt.iscte.poo.utils.Point2D;
 
-public abstract class StaticObject extends GameObject {
+public abstract class StaticObject extends GameObject { // Classe abstrata que herda de GameObject
 
     public StaticObject(Point2D p, String img) {
-        super(p);
-        this.imageName = img;
+        super(p); // Chama o construtor da superclasse GameObject para inicializar a posição
+        this.imageName = img; // Define o nome da imagem do objeto
     }
 
-    public boolean aguentaPeso() {
+    public boolean aguentaPeso() { // Default é que aguenta peso
         return true;
     }
 
-    public boolean podeAtravessar(GameCharacter character) {
+    public boolean podeAtravessar(GameCharacter character) { // Default é que não pode atravessar
         return false;
     }
 
-    public boolean leve() {
+    public boolean leve() { // Default é que não é leve
         return false;
     }
 
-    public boolean movel() {
+    public boolean movel() { // Default é que não é móvel
         return false;
     }
 
-    public boolean podeSerEmpurrado(Direction dir) {
+    public boolean podeSerEmpurrado(Direction dir) { // Default é que pode ser empurrado em qualquer direção
         return true;
     }
 
-    public void notificarEmpurrado() {
+    public void notificarEmpurrado() { // Cada classe pode implementar este método se precisar de fazer algo quando for
+                                       // empurrada
 
     }
 
